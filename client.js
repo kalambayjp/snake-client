412573f3d2c525187eef44c33901e6c99ca4e01b
JP <kalambayjp@gmail.com>
2021-12-01 15:24:33 -0800

    added a connection message on client side and sent a name message to server

--- client.js
+++ client.js
@@ -9,6 +9,12 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  // log a conneted message on connection
+  conn.on('connect', () => {
+    console.log('Succesfully connected to server');
+    conn.write('Name: JPK');
+
+  });
   // accept incoming data from server and log to console
   conn.on('data', (data) => {
     console.log(data);
